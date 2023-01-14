7ee15d3ed01beed0710b1c52a7acbe93f1c5eb87
Victor Guedes <mirandaguedes.victor@gmail.com>
2023-01-14 13:34:01 -0300

    Refactor, add method to a class

--- scanner_port.rb
+++ scanner_port.rb
@@ -1,16 +1,17 @@
 require 'socket'
-
-def scan_func(host,port)
-    begin
-        socket = TCPSocket.new(host, port)
-        status = "open"
-    rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT
-        status = "closed"
+class Scanner_port
+    def scan(host,port)
+        begin
+            socket = TCPSocket.new(host, port)
+            status = "open"
+        rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT
+            status = "closed"
+        end
+        puts "Port #{port} is #{status}."
     end
-    puts "Port #{port} is #{status}."
 end
 
 host, *ports = ARGV
-ports.each{|port| scan_func(host,port)}
+ports.each{|port| scan(host,port)}
 
 
